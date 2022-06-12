46b9b7533f664b9e54d5edbe8fc25c6ed092a9b5
최원석 <cws653@naver.com>
2022-06-12 23:52:02 +0900

    RxSwift+MVVM 스터디 자료 업로드

---
+++ RxSwift_In_4_Hours-master 2/Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  RxSwift+MVVM
+//
+//  Created by iamchiwon on 05/08/2019.
+//  Copyright © 2019 iamchiwon. All rights reserved.
+//
+
+import RxSwift
+import SwiftyJSON
+import UIKit
+
+let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
+
+class 나중에생기는데이터<T> {
+    private let task: (@escaping (T) -> Void) -> Void
+
+    init(task: @escaping (@escaping (T) -> Void) -> Void) {
+        self.task = task
+    }
+
+    func 나중에오면(_ f: @escaping (T) -> Void) {
+        task(f)
+    }
+}
+
+class ViewController: UIViewController {
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var editView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
+        }
+    }
+
+    private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
+        guard let v = v else { return }
+        UIView.animate(withDuration: 0.3, animations: { [weak v] in
+            v?.isHidden = !s
+        }, completion: { [weak self] _ in
+            self?.view.layoutIfNeeded()
+        })
+    }
+
+    // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
+    func downloadJson(_ url: String) -> Observable<String?> {
+        return Observable.create { f in
+            DispatchQueue.global().async {
+                let url = URL(string: url)!
+                let data = try! Data(contentsOf: url)
+                let json = String(data: data, encoding: .utf8)
+
+                DispatchQueue.main.async {
+                    f.onNext(json)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+//    return 나중에생기는데이터() { f in
+//        DispatchQueue.global().async {
+//            let url = URL(string: url)!
+//            let data = try! Data(contentsOf: url)
+//            let json = String(data: data, encoding: .utf8)
+//            DispatchQueue.main.async {
+//                f(json)
+//            }
+//        }
+//    }
+
+//    func downloadJson(_ url: String) -> 나중에생기는데이터<String?> {
+//        return 나중에생기는데이터() { f in
+//            DispatchQueue.global().async {
+//                let url = URL(string: url)!
+//                let data = try! Data(contentsOf: url)
+//                let json = String(data: data, encoding: .utf8)
+//                DispatchQueue.main.async {
+//                    f(json)
+//                }
+//            }
+//        }
+//    }
+
+    // MARK: SYNC
+
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+
+    @IBAction func onLoad() {
+        editView.text = ""
+        self.setVisibleWithAnimation(self.activityIndicator, true)
+
+        // 2. Observable로 오는 데이터를 받아서 처리하는 방법
+        downloadJson(MEMBER_LIST_URL)
+            .subscribe { event in
+                switch event {
+                case let .next(json):
+                    self.editView.text = json
+                    self.setVisibleWithAnimation(self.activityIndicator, false)
+                case .completed: break
+                case .error: break
+                }
+            }
+
+
+        
+
+
+//        let json: 나중에생기는데이터<String?> = downloadJson(MEMBER_LIST_URL)
+
+        //        json.나중에오면 { json in
+        //                        self.editView.text = json
+        //                        self.setVisibleWithAnimation(self.activityIndicator, false)
+        //        }
+
+
+        //        downloadJson(MEMBER_LIST_URL) { json in
+        //            self.editView.text = json
+        //            self.setVisibleWithAnimation(self.activityIndicator, false)
+        //        }
+    }
+}
